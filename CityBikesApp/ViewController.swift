3d47669e675c2fd49b52510530b3edba4413d225
venkatesh <venkatesh.garipally@gmail.com>
2017-12-08 18:50:44 +0530

    tableview implemented partially

--- CityBikesApp/ViewController.swift
+++ CityBikesApp/ViewController.swift
@@ -8,12 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class BikeListTableViewCell: UITableViewCell {
+    @IBOutlet weak var bikeTitleLabel:UILabel!
+    @IBOutlet weak var companyLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+}
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+
+    @IBOutlet weak var bikeListTableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
+        // Do any additional setup after loading the view, typically from a nib
+        self.title = "CityBikes"
+        getBikeList()
+    }
+    
+    func getBikeList(){
         let serviceHelper = CBWebServiceHelper.shared()
         serviceHelper.sendRequest(toWebServer:"http://api.citybik.es/v2/networks?fields=id,name,company,location") {(_status, data, response) in
             do {
@@ -27,6 +39,22 @@ class ViewController: UIViewController {
         }
     }
 
+    // MARK:- TableView Delegate and Datasource methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BikeListTableViewCell") as! BikeListTableViewCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
